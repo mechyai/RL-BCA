0c8050a74c8d376633f5b08b00b8eaf65d03aae6
Chris (mechyai) <jpeubel.ai@gmail.com>
2021-07-15 09:19:08 -0400

    v1 of model plotter

--- EmsPy/model_debugger.py
+++ EmsPy/model_debugger.py
@@ -11,7 +11,7 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 ep_path = 'A:/Programs/EnergyPlusV9-5-0/'
 ep_idf_to_run = 'A:/Files/PycharmProjects/RL-BCA/OpenStudio_Models/idf_files/5office_small_ems.idf'
-ep_weather_path = 'A:/Files/PycharmProjects/RL-BCA/Resource_Files/reference_weather/2A_USA_TX_HOUSTON_TMY2.epw'
+ep_weather_path = 'A:/Files/PycharmProjects/RL-BCA/OpenStudio_Models/5office_small_ems/files/USA_FL_Tampa-MacDill.AFB.747880_TMY3.epw'
 cvs_output_path = 'default_dfs'
 
 # --- create EMS Table of Contents (TC) for sensors/actuators ---
@@ -56,21 +56,6 @@ timesteps = 30
 # create calling point with actuation function
 calling_point = 'callback_after_predictor_after_hvac_managers'
 
-# create building energy simulation obj
-sim = emspy.BcaEnv(ep_path, ep_idf_to_run, timesteps, vars_tc, int_vars_tc, meters_tc, actuators_tc, weather_tc)
-# create RL agent obj
-# agent = Agent()
-
-sim.set_calling_point_and_callback_function(calling_point, None, None, True, 1, 1)
-sim.run_env(ep_weather_path)
-sim.reset_state()
-
-dfs = sim.get_df(to_csv_file=cvs_output_path)
-
-
-
-
-
 class Agent:
     def __init__(self):
         self.day = ''
@@ -95,10 +80,27 @@ class Agent:
 
         # print(f'Action:  Timestep:{timestep}, DBT Act: {update_dbt}, DBT Current: {dbt}\n')
 
-        return {'heating_sp': 20, 'cooling_sp': 22}
+        return {'zone0_heat_sp': 23, 'zone0_cool_sp': 29,
+                'zone1_heat_sp': 23, 'zone1_cool_sp': 29,
+                'zone2_heat_sp': 23, 'zone2_cool_sp': 29,
+                'zone3_heat_sp': 23, 'zone3_cool_sp': 29,
+                'zone4_heat_sp': 23, 'zone4_cool_sp': 29}
 
     def c_to_f(self, temp_c: float):
         return 1.8 * temp_c + 32
+
+
+# create building energy simulation obj
+sim = emspy.BcaEnv(ep_path, ep_idf_to_run, timesteps, vars_tc, int_vars_tc, meters_tc, actuators_tc, weather_tc)
+# create RL agent obj
+agent = Agent()
+
+sim.set_calling_point_and_callback_function(calling_point, None, agent.act, True, 1, 1)
+sim.run_env(ep_weather_path)
+sim.reset_state()
+
+dfs = sim.get_df(to_csv_file=cvs_output_path)
+
 
 
 
--- EmsPy/model_plotter.py
+++ EmsPy/model_plotter.py
@@ -1,4 +1,86 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import pandas as pd
+import numpy as np
+from datetime import datetime
+import datetime as dt
 
 df = pd.read_csv('A:\Files\PycharmProjects\RL-BCA\EmsPy\default_dfs')
+
+# trim to ignore Design Days, truncate for faster dev
+dd_index = 1552
+dd_span = 20000
+df = df.truncate(before=dd_index, after=dd_index + dd_span)
+
+# replace year with same leap-year (IF NEEDED)
+timex = df['Datetime'].str.slice_replace(stop=5, repl='2000-')  # fix inconsistent years of weather file
+
+# timex = df['Datetime']
+# transform str back into datetime obj
+# timex = list(map(datetime.strptime, timex, len(timex)*['%m-%d %H:%M:%S']))
+timex = list(map(datetime.strptime, timex, len(timex)*['%Y-%m-%d %H:%M:%S']))
+
+# timex = range(df.shape[0])
+
+
+fig, (ax1, ax2) = plt.subplots(2)
+fig.suptitle('Temperature and Humidity Zone Control')
+
+# temp dates
+ax1.set_xlabel('')
+formatter = mdates.DateFormatter("%m-%d")
+ax1.xaxis.set_major_formatter(formatter)
+locator = mdates.DayLocator()
+ax1.xaxis.set_major_locator(locator)
+# do above ^ just to get proper tick spacing
+ax1.set_xticklabels([])  # share x-axis, hide x-axis
+ax1.grid()
+fig.subplots_adjust(hspace=0.1)
+# humidity dates
+ax2.set_xlabel('Date')
+formatter = mdates.DateFormatter("%m-%d")
+ax2.xaxis.set_major_formatter(formatter)
+locator = mdates.DayLocator()
+ax2.xaxis.set_major_locator(locator)
+ax2.grid()
+
+# xlims
+ax1.set_xlim([datetime(2000, 1, 1,0,0,0), datetime(2000,1,30,0,0,0)])
+ax2.set_xlim([datetime(2000, 1, 1,0,0,0), datetime(2000,1,30,0,0,0)])
+
+
+# line plot params
+outdoor_linewidth = 2
+outdoor_color = 'k'
+
+# zone temps
+ax1.set_ylabel('Temp (c)')
+ax1.plot(timex, df['zone2_temp'].values, label='Zone2 Temp')
+ax1.plot(timex, df['zone3_temp'].values, label='Zone3 Temp')
+ax1.plot(timex, df['zone4_temp'].values, label='Zone4 Temp')
+ax1.plot(timex, df['zone1_temp'].values, label='Zone1 Temp')
+ax1.plot(timex, df['zone0_temp'].values, label='Zone0 Temp')
+# outdoor
+ax1.plot(timex, df['oa_db_temp'].values, outdoor_color, label='Outdoor DB Temp', linewidth=outdoor_linewidth)
+ax1.legend()
+
+# zone humidity
+ax2.set_ylabel('Relative Humidity (%)')
+ax2.plot(timex, df['zone0_rh'].values, label='Zone0 %RH')
+ax2.plot(timex, df['zone1_rh'].values, label='Zone1 %RH')
+ax2.plot(timex, df['zone2_rh'].values, label='Zone2 %RH')
+ax2.plot(timex, df['zone3_rh'].values, label='Zone3 %RH')
+ax2.plot(timex, df['zone4_rh'].values, label='Zone4 %RH')
+# outdoor
+ax2.plot(timex, df['oa_rh'].values, outdoor_color, label='Outdoor %RH', linewidth=outdoor_linewidth)
+ax2.legend()
+
+# control # TODO match control line type to its zone line type
+ax1.fill_between(timex, df['zone0_cool_sp'].values, df['zone0_heat_sp'].values )
+
+
+plt.setp(ax1.xaxis.get_majorticklabels(), rotation=45)
+plt.setp(ax2.xaxis.get_majorticklabels(), rotation=45)
+
+
+plt.show()
