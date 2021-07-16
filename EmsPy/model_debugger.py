d5f9114c1d97ac40be08567201d82b05d59769c4
Chris (mechyai) <jpeubel.ai@gmail.com>
2021-07-15 23:05:54 -0400

    various idfs and plots

--- EmsPy/model_debugger.py
+++ EmsPy/model_debugger.py
@@ -10,9 +10,9 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 # OMP: Error #15: Initializing libiomp5md.dll, but found libiomp5md.dll already initialized.
 
 ep_path = 'A:/Programs/EnergyPlusV9-5-0/'
-ep_idf_to_run = 'A:/Files/PycharmProjects/RL-BCA/OpenStudio_Models/idf_files/5office_small_ems.idf'
+ep_idf_to_run = 'A:/Files/PycharmProjects/RL-BCA/OpenStudio_Models/idf_files/5office_small_ems_all_on.idf'
 ep_weather_path = 'A:/Files/PycharmProjects/RL-BCA/OpenStudio_Models/5office_small_ems/files/USA_FL_Tampa-MacDill.AFB.747880_TMY3.epw'
-cvs_output_path = 'default_dfs'
+cvs_output_path = 'electricity_dfs'
 
 # --- create EMS Table of Contents (TC) for sensors/actuators ---
 # vars_tc = {"attr_handle_name": ["variable_type", "variable_key"],...}
@@ -20,32 +20,51 @@ cvs_output_path = 'default_dfs'
 # meters_tc = {"attr_handle_name": "meter_name",...}
 # actuators_tc = {"attr_handle_name": ["component_type", "control_type", "actuator_key"],...}
 # weather_tc = {"attr_name": "weather_metric",...}
-vars_tc = {
-    'oa_wb_temp': ['Zone Outdoor Air Wetbulb Temperature', 'Core_ZN ZN'],
-    'zone0_temp': ['Zone Air Temperature', 'Core_ZN ZN'],
-    'zone1_temp': ['Zone Air Temperature', 'Perimeter_ZN_1 ZN'],
-    'zone2_temp': ['Zone Air Temperature', 'Perimeter_ZN_2 ZN'],
-    'zone3_temp': ['Zone Air Temperature', 'Perimeter_ZN_3 ZN'],
-    'zone4_temp': ['Zone Air Temperature', 'Perimeter_ZN_4 ZN'],
-    'zone0_rh': ['Zone Air Relative Humidity', 'Core_ZN ZN'],
-    'zone1_rh': ['Zone Air Relative Humidity', 'Perimeter_ZN_1 ZN'],
-    'zone2_rh': ['Zone Air Relative Humidity', 'Perimeter_ZN_2 ZN'],
-    'zone3_rh': ['Zone Air Relative Humidity', 'Perimeter_ZN_3 ZN'],
-    'zone4_rh': ['Zone Air Relative Humidity', 'Perimeter_ZN_4 ZN'],
-}
 int_vars_tc = None
 meters_tc = None
+vars_tc = {
+    # zones temps
+    'z0_temp': ['Zone Air Temperature', 'Core_ZN ZN'],
+    'z1_temp': ['Zone Air Temperature', 'Perimeter_ZN_1 ZN'],
+    'z2_temp': ['Zone Air Temperature', 'Perimeter_ZN_2 ZN'],
+    'z3_temp': ['Zone Air Temperature', 'Perimeter_ZN_3 ZN'],
+    'z4_temp': ['Zone Air Temperature', 'Perimeter_ZN_4 ZN'],
+    'z0_rh': ['Zone Air Relative Humidity', 'Core_ZN ZN'],
+    # zones %rh
+    'z1_rh': ['Zone Air Relative Humidity', 'Perimeter_ZN_1 ZN'],
+    'z2_rh': ['Zone Air Relative Humidity', 'Perimeter_ZN_2 ZN'],
+    'z3_rh': ['Zone Air Relative Humidity', 'Perimeter_ZN_3 ZN'],
+    'z4_rh': ['Zone Air Relative Humidity', 'Perimeter_ZN_4 ZN'],
+    # people count
+    'z0_ppl': ['Zone People Occupant Count', 'Core_ZN ZN'],
+    'z1_ppl': ['Zone People Occupant Count', 'Perimeter_ZN_1 ZN'],
+    'z2_ppl': ['Zone People Occupant Count', 'Perimeter_ZN_2 ZN'],
+    'z3_ppl': ['Zone People Occupant Count', 'Perimeter_ZN_3 ZN'],
+    'z4_ppl': ['Zone People Occupant Count', 'Perimeter_ZN_4 ZN'],
+    # energy
+    'z0_e': ['Unitary System Electricity Energy', 'Core_ZN ZN'],
+    'z1_e': ['Unitary System Electricity Energy', 'Perimeter_ZN_1 ZN'],
+    'z2_e': ['Unitary System Electricity Energy', 'Perimeter_ZN_2 ZN'],
+    'z3_e': ['Unitary System Electricity Energy', 'Perimeter_ZN_3 ZN'],
+    'z4_e': ['Unitary System Electricity Energy', 'Perimeter_ZN_4 ZN'],
+    # energy rate
+    'z0_e_rate': ['Unitary System Electricity Rate', 'Core_ZN ZN'],
+    'z1_e_rate': ['Unitary System Electricity Rate', 'Perimeter_ZN_1 ZN'],
+    'z2_e_rate': ['Unitary System Electricity Rate', 'Perimeter_ZN_2 ZN'],
+    'z3_e_rate': ['Unitary System Electricity Rate', 'Perimeter_ZN_3 ZN'],
+    'z4_e_rate': ['Unitary System Electricity Rate', 'Perimeter_ZN_4 ZN']
+}
 actuators_tc = {
-    'zone0_cool_sp': ['Zone Temperature Control', 'Cooling Setpoint', 'Core_ZN ZN'],
-    'zone1_cool_sp': ['Zone Temperature Control', 'Cooling Setpoint', 'Perimeter_ZN_1 ZN'],
-    'zone2_cool_sp': ['Zone Temperature Control', 'Cooling Setpoint', 'Perimeter_ZN_2 ZN'],
-    'zone3_cool_sp': ['Zone Temperature Control', 'Cooling Setpoint', 'Perimeter_ZN_3 ZN'],
-    'zone4_cool_sp': ['Zone Temperature Control', 'Cooling Setpoint', 'Perimeter_ZN_4 ZN'],
-    'zone0_heat_sp': ['Zone Temperature Control', 'Heating Setpoint', 'Core_ZN ZN'],
-    'zone1_heat_sp': ['Zone Temperature Control', 'Heating Setpoint', 'Perimeter_ZN_1 ZN'],
-    'zone2_heat_sp': ['Zone Temperature Control', 'Heating Setpoint', 'Perimeter_ZN_2 ZN'],
-    'zone3_heat_sp': ['Zone Temperature Control', 'Heating Setpoint', 'Perimeter_ZN_3 ZN'],
-    'zone4_heat_sp': ['Zone Temperature Control', 'Heating Setpoint', 'Perimeter_ZN_4 ZN'],
+    'z0_cool_sp': ['Zone Temperature Control', 'Cooling Setpoint', 'Core_ZN ZN'],
+    'z1_cool_sp': ['Zone Temperature Control', 'Cooling Setpoint', 'Perimeter_ZN_1 ZN'],
+    'z2_cool_sp': ['Zone Temperature Control', 'Cooling Setpoint', 'Perimeter_ZN_2 ZN'],
+    'z3_cool_sp': ['Zone Temperature Control', 'Cooling Setpoint', 'Perimeter_ZN_3 ZN'],
+    'z4_cool_sp': ['Zone Temperature Control', 'Cooling Setpoint', 'Perimeter_ZN_4 ZN'],
+    'z0_heat_sp': ['Zone Temperature Control', 'Heating Setpoint', 'Core_ZN ZN'],
+    'z1_heat_sp': ['Zone Temperature Control', 'Heating Setpoint', 'Perimeter_ZN_1 ZN'],
+    'z2_heat_sp': ['Zone Temperature Control', 'Heating Setpoint', 'Perimeter_ZN_2 ZN'],
+    'z3_heat_sp': ['Zone Temperature Control', 'Heating Setpoint', 'Perimeter_ZN_3 ZN'],
+    'z4_heat_sp': ['Zone Temperature Control', 'Heating Setpoint', 'Perimeter_ZN_4 ZN'],
 }
 weather_tc = {
     'oa_db_temp': 'outdoor_dry_bulb',
@@ -80,11 +99,13 @@ class Agent:
 
         # print(f'Action:  Timestep:{timestep}, DBT Act: {update_dbt}, DBT Current: {dbt}\n')
 
-        return {'zone0_heat_sp': 23, 'zone0_cool_sp': 29,
-                'zone1_heat_sp': 23, 'zone1_cool_sp': 29,
-                'zone2_heat_sp': 23, 'zone2_cool_sp': 29,
-                'zone3_heat_sp': 23, 'zone3_cool_sp': 29,
-                'zone4_heat_sp': 23, 'zone4_cool_sp': 29}
+        return {
+            'z0_heat_sp': 23, 'z0_cool_sp': 29,
+            'z1_heat_sp': 23, 'z1_cool_sp': 29,
+            'z2_heat_sp': 23, 'z2_cool_sp': 29,
+            'z3_heat_sp': 23, 'z3_cool_sp': 29,
+            'z4_heat_sp': 23, 'z4_cool_sp': 29
+            }
 
     def c_to_f(self, temp_c: float):
         return 1.8 * temp_c + 32
--- EmsPy/model_plotter.py
+++ EmsPy/model_plotter.py
@@ -5,14 +5,16 @@ import numpy as np
 from datetime import datetime
 import datetime as dt
 
-df = pd.read_csv('A:\Files\PycharmProjects\RL-BCA\EmsPy\default_dfs')
+df_file = 'default_dfs'
+
+df = pd.read_csv('A:\Files\PycharmProjects\RL-BCA\EmsPy\\' + df_file )
 
 # trim to ignore Design Days, truncate for faster dev
-dd_index = 1552
-dd_span = 20000
+dd_index = 0
+dd_span = 10000
 df = df.truncate(before=dd_index, after=dd_index + dd_span)
 
-# replace year with same leap-year (IF NEEDED)
+# replace year with same leap-year (IF NEEDED), https://www.timeanddate.com/date/weekday.html
 timex = df['Datetime'].str.slice_replace(stop=5, repl='2000-')  # fix inconsistent years of weather file
 
 # timex = df['Datetime']
@@ -45,8 +47,8 @@ ax2.xaxis.set_major_locator(locator)
 ax2.grid()
 
 # xlims
-ax1.set_xlim([datetime(2000, 1, 1,0,0,0), datetime(2000,1,30,0,0,0)])
-ax2.set_xlim([datetime(2000, 1, 1,0,0,0), datetime(2000,1,30,0,0,0)])
+ax1.set_xlim([datetime(2000, 1, 1,0,0,0), datetime(2000,1,15,0,0,0)])
+ax2.set_xlim([datetime(2000, 1, 1,0,0,0), datetime(2000,1,15,0,0,0)])
 
 
 # line plot params
@@ -55,29 +57,30 @@ outdoor_color = 'k'
 
 # zone temps
 ax1.set_ylabel('Temp (c)')
-ax1.plot(timex, df['zone2_temp'].values, label='Zone2 Temp')
-ax1.plot(timex, df['zone3_temp'].values, label='Zone3 Temp')
-ax1.plot(timex, df['zone4_temp'].values, label='Zone4 Temp')
-ax1.plot(timex, df['zone1_temp'].values, label='Zone1 Temp')
-ax1.plot(timex, df['zone0_temp'].values, label='Zone0 Temp')
+ax1.plot(timex, df['z2_temp'].values, label='Z2 Temp')
+ax1.plot(timex, df['z3_temp'].values, label='Z3 Temp')
+ax1.plot(timex, df['z4_temp'].values, label='Z4 Temp')
+ax1.plot(timex, df['z1_temp'].values, label='Z1 Temp')
+ax1.plot(timex, df['z0_temp'].values, label='Z0 Temp')
 # outdoor
 ax1.plot(timex, df['oa_db_temp'].values, outdoor_color, label='Outdoor DB Temp', linewidth=outdoor_linewidth)
 ax1.legend()
 
 # zone humidity
 ax2.set_ylabel('Relative Humidity (%)')
-ax2.plot(timex, df['zone0_rh'].values, label='Zone0 %RH')
-ax2.plot(timex, df['zone1_rh'].values, label='Zone1 %RH')
-ax2.plot(timex, df['zone2_rh'].values, label='Zone2 %RH')
-ax2.plot(timex, df['zone3_rh'].values, label='Zone3 %RH')
-ax2.plot(timex, df['zone4_rh'].values, label='Zone4 %RH')
+ax2.plot(timex, df['z0_rh'].values, label='Z0 %RH')
+ax2.plot(timex, df['z1_rh'].values, label='Z1 %RH')
+ax2.plot(timex, df['z2_rh'].values, label='Z2 %RH')
+ax2.plot(timex, df['z3_rh'].values, label='Z3 %RH')
+ax2.plot(timex, df['z4_rh'].values, label='Z4 %RH')
 # outdoor
 ax2.plot(timex, df['oa_rh'].values, outdoor_color, label='Outdoor %RH', linewidth=outdoor_linewidth)
 ax2.legend()
 
 # control # TODO match control line type to its zone line type
-ax1.fill_between(timex, df['zone0_cool_sp'].values, df['zone0_heat_sp'].values )
-
+# RGBA color picker https://www.w3schools.com/css/css_colors_rgb.asp
+# only shows on 1 monitor
+ax1.fill_between(timex, df['zone0_cool_sp'].values, df['zone0_heat_sp'].values, color=(240/255, 240/255, 240/255, 1))
 
 plt.setp(ax1.xaxis.get_majorticklabels(), rotation=45)
 plt.setp(ax2.xaxis.get_majorticklabels(), rotation=45)
